85823fd56b7a58672725a261b7bfe1c10467cd68
Bianca <biancancanal@gmail.com>
2021-03-08 18:51:09 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+function clickAlert() {
+    alert('I was clicked!');
+  }
+
 function addingEventListener() {
+    const input = document.getElementById('input');
+    input.addEventListener('click', clickAlert); 
 }
+
+
+
+
+
